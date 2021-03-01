9585defa9e64b61498a29eda0ea20e195f82a8b4
Djumabaevs <76224728+Djumabaevs@users.noreply.github.com>
2021-03-01 20:31:27 +0600

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,12 +1,7 @@
-package ExPackageFeb5;
+package ExPackageFeb6;
 
 
-
-
-class Doing6 {
-
-}
-public class Employee{
+class Employee {
     public String name;
     public int age;
     public String toString(){
@@ -14,69 +9,3 @@ public class Employee{
     }
 }
 
-class StoredEmployee{
-public String key;
-public Employee employee;
-public StoredEmployee(String key, Employee employee){
-    this.key = key;
-    this.employee = employee;
-}
-}
-
-class HashTable{
-    private Employee[] hashtable;
-
-    public HashTable(){
-        hashtable = new Employee[10];
-    }
-    public void put(String key, Employee employee){
-        int hashedKey = hashKey(key);
-        if(hashtable[hashedKey] != null){
-            System.out.println("Sorry the position is not empty: " + hashedKey);
-        } else {
-            hashtable[hashedKey] = employee;
-        }
-    }
-
-    public int hashKey(String key){
-        return key.length() % hashtable.length;
-    }
-
-    public Employee get(String key){
-        int hashedKey = hashKey(key);
-        return hashtable[hashedKey];
-    }
-
-    public void printHashtable(){
-        for (int i = 0; i < hashtable.length; i++) {
-            System.out.println(hashtable[i]);
-        }
-    }
-
-    private boolean occupied(int index){
-        return hashtable[index] != null;
-    }
-
-    public void putOptimized(String key, Employee employee){
-        int hashedKey = hashKey(key);
-        if(occupied(hashedKey)){
-            int stopIndex = hashedKey;
-            if(hashedKey == hashtable.length - 1){
-                hashedKey = 0;
-            } else {
-                hashedKey++;
-            }
-            while(occupied(hashedKey) && hashedKey != stopIndex){
-                hashedKey = (hashedKey  +1) % hashtable.length;
-            }
-        }
-
-        if(occupied(hashedKey)){
-            System.out.println("Sorry, this place is occipied!" + hashedKey);
-        } else {
-            hashtable[hashedKey] = employee;
-        }
-    }
-
-
-}

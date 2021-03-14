c0acf7281825e298ae507421cfd45e272501ad29
Djumabaevs <76224728+Djumabaevs@users.noreply.github.com>
2021-03-14 09:02:36 +0600

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,21 +1,50 @@
-package ExPackageJan4.Programmer1;
+package Lists;
 
-import java.io.Serializable;
+import java.util.Objects;
 
-public class Car implements Serializable {
- String model;
- int engine;
+public class Car {
+    private String model;
+    private int year;
 
-    public Car(String model, int engine) {
+    public Car(String model, int year) {
         this.model = model;
-        this.engine = engine;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return getYear() == car.getYear() && getModel().equals(car.getModel());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getModel(), getYear());
     }
 
     @Override
     public String toString() {
         return "Car{" +
                 "model='" + model + '\'' +
-                ", engine=" + engine +
+                ", year=" + year +
                 '}';
     }
 }
